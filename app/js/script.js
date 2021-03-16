fd6deff62fc9f1e85719b766cda526223ecb3e46
Stepan <30976192+Kerekesh@users.noreply.github.com>
2021-03-16 21:28:17 +0200

    add training13 - JS Intro

---
+++ app/js/script.js
@@ -0,0 +1,14 @@
+let i = 0;
+
+if (i === 0) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+console.log(i==0?true:false);
+
+let a = 6; b = 3;
+console.log(a > b?true:false);
+
+let str = prompt("Input your digit", 0);
+alert(str);
