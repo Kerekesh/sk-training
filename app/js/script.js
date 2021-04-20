e6bf964b57e4a2ca94343893a8485022df13904c
Stepan <30976192+Kerekesh@users.noreply.github.com>
2021-04-20 19:10:45 +0300

    add training22

--- app/js/script.js
+++ app/js/script.js
@@ -2,19 +2,47 @@
 //     alert(this.innerHTML);
 // };
 
+// const { Dropdown } = require("bootstrap");
+
 // // btn.onclick = function() {
 // //     alert('Hello world 3');
 // // };
 
-let evnt = "click";
-btn.addEventListener(evnt, showValue);
 
-function showValue(){
-    console.log(event);
-    if(event.altKey) {
-        console.log('Alt pressed');
-    } else {
-        console.log('Don`t pressed');
-    }
-    console.log(event.type);
+// // добавить обработчик на кнопку, при нажатии на которую будет в консоль
+// // выводиться текст с поля
+
+// let evnt = "click";
+// btn.addEventListener(evnt, showValue);
+
+// function showValue(e){
+//     console.log(e)
+//     console.log(text.value)
+//     if(e.altKey) { //проверка нажатия клавиши Alt
+//         console.log('Alt pressed');
+//     } else {
+//         console.log('Alt don`t pressed');
+//     }
+// }
+
+
+// ● написать обработчик на событие клик чтобы элемент перемещался на
+// координаты клика
+// ● сделать dropdown (выпадающий список)
+
+
+// document.body.onclick = function(event) {
+//     let box = document.getElementById("box");
+//     box.style.left = event.clientX-50+"px";
+//     box.style.top = event.clientY-50+"px";
+// };
+
+
+dropdown.addEventListener("click", showItems, false);
+let toggle = document.querySelectorAll(".none");
+
+function showItems(e) {
+  for (let i = 0; i < toggle.length; i++) {
+    toggle[i].classList.toggle("none");
+  }
 }
