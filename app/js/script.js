727f3694d8eca7eee7a9c20c4475aaf7b2dd867f
Stepan <30976192+Kerekesh@users.noreply.github.com>
2021-04-25 23:26:34 +0300

    add homework tasks

--- app/js/script.js
+++ app/js/script.js
@@ -62,11 +62,42 @@
 //   event.target.removeAttribute("style");
 // }
 
-window.addEventListener("scroll", toggleClass);
 
-function toggleClass(event){
-  console.log(this.scrollY);
-  if (this.scrollY > 200) {
-    header.classList.toggle("green");
-  }
-}
+
+// // ● при прокрутке страницы на 200px тоглить класс в header
+
+// window.addEventListener("scroll", toggleClass);
+
+// function toggleClass(){
+//   for (let i = 0; i < document.body.clientHeight; i+=200) {
+//     if (window.scrollY == i) {
+//       header.classList.toggle("green");
+//     }
+//   }
+// }
+
+
+// // ● сделать проверку на ширину экрана, если меньше 768px то к элементу
+// // добавить класс
+
+// window.addEventListener("resize", addClass);
+
+// function addClass(event) {
+//   // console.log(event);
+//   if (window.innerWidth < 768) {
+//     header.classList.add("green");
+//   } else {
+//     header.classList.remove("green");
+//   }
+// }
+
+
+// // ● сделать интуп (type = "text") в который можно вводить только цифры
+// // (keyCode для чисел от 48 до 57 и от 96 до 105)
+
+// keynumbers.addEventListener("keydown", function(event){
+//   let key = event.which;
+//   if (key < 48 || key > 57) {
+//     event.preventDefault();
+//   }
+// })
