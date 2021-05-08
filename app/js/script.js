c62836fae72448075d35603b1b36edcbdf47c6c4
Stepan <30976192+Kerekesh@users.noreply.github.com>
2021-05-08 21:34:53 +0300

    add tasks in training26

--- app/js/script.js
+++ app/js/script.js
@@ -1,21 +1,103 @@
-class User {
-  constructor(name) {
-    this.name = name; // вызывают сеттер
-  }
-  get name() {
-    return this._name;
-  }
-  set name(value) {
-    if (value.length < 4) {
-      alert("имя короткое");
-      return;
-    }
-    this._name = value;
-  }
-}
-let user = new User("John");
-alert(user.name); // John
-user = new User(""); // имя короткое
+// const animal = {
+//   name: "Animal",
+//   age: 5,
+//   hasTail: true
+// }
+
+// class Animal {
+
+// static type = "ANIMAL";
+
+//   constructor(options) {
+//     this.name = options.name;
+//     this.age = options.age;
+//     this.hasTail = options.hasTail;
+//   }
+
+//   voice() {
+//     console.log("Im Animal")
+//   }
+// }
+
+// // const animal = new Animal({
+// //   name: "Animal",
+// //   age: 5,
+// //   hasTail: true
+// // });
+
+// class Cat extends Animal {
+//   static type = "CAT";
+
+//   constructor(options) {
+//     super(options);
+//     this.color = options.color;
+//   }
+
+//   voice() {
+//     super.voice();
+//     console.log("I am Cat");
+//   }
+
+//   get ageInfo() {
+//     return this.age * 7;
+//   }
+//   set ageInfo(newAge) {
+//     this.age = newAge
+//   }
+// }
+
+// const cat = new Cat({
+//   name: 'Cat',
+//   age: 7,
+//   hasTail: true,
+//   color: "black"
+// });
+
+
+// const person = Object.create({
+//   calculateAge() {
+//     console.log("Age", new Date().getFullYear() - this.birthYear)
+//   }
+// }, {
+//   name: {
+//     value: "Stepan",
+//     enumerable: true
+//   },
+//   birthYear: {
+//     value: 1997,
+//     enumerable: false // по умолчанию 
+//   },
+//   age: {
+//     get(){
+//       return new Date().getFullYear() - this.birthYear
+//     },
+//     set(value){
+//       console.log("Set age", value)
+//     }
+//   }
+// });
+
+// person.name = "Michael"; 
+// console.log(person);
+
+// class User {
+//   constructor(name) {
+//     this.name = name; // вызывают сеттер
+//   }
+//   get name() {
+//     return this._name;
+//   }
+//   set name(value) {
+//     if (value.length < 4) {
+//       alert("имя короткое");
+//       return;
+//     }
+//     this._name = value;
+//   }
+// }
+// let user = new User("John");
+// alert(user.name); // John
+// user = new User(""); // имя короткое
 
 
 // class User {
